d737276da9972b3019616b7ab58333f5f96412fe
syzwniafifh <nursyazwaniafifah@gmail.com>
2024-10-10 16:07:11 +0800

    Initial commit

---
+++ YoutubeApiProject1/Controllers/YouTubeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YouTubeApiProject1.Models;
+using YouTubeApiProject1.Services;
+
+namespace YouTubeApiProject1.Controllers
+{
+    public class YouTubeController : Controller
+    {
+        private readonly YouTubeApiService _youtubeService;
+        public YouTubeController(YouTubeApiService
+       youtubeService)
+        {
+            _youtubeService = youtubeService;
+        }
+        // Display Search Page
+        public IActionResult Index()
+        {
+            return View(new List<YouTubeVideoModel>());
+        }
+        // Handle search query
+        [HttpPost]
+        public async Task<IActionResult> Search(string query)
+        {
+            var videos = await
+           _youtubeService.SearchVideosAsync(query);
+            return View("Index", videos);
+        }
+    }
+}
---
+++ YoutubeApiProject1/Models/YouTubeVideoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace YouTubeApiProject1.Models
+{
+    public class YouTubeVideoModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string VideoUrl { get; set; } // Add this property for the video link
+
+    }
+}
+
